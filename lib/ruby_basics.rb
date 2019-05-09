ef3f4b6e1b184b5fc45f79706b73ab8754643291
interpreted-refactoring-0526 <taungon@brandeis.edu>
2019-05-09 09:22:15 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
 def argue
-  
+  argument
 end
 
 def greeting
-
+ puts "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+  "Cool"
 end
 
 def last_evaluated_value
